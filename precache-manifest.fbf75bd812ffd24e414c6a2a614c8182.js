a75da2019e88fa91e30ba5b0f5dc76b90e6c155c
Евгений <evgenij.romanko@neoseo.com.ua>
2020-07-05 19:44:00 +0300

    Updates

---
+++ precache-manifest.fbf75bd812ffd24e414c6a2a614c8182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba197960120bead5a750e68635b417b4",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "46457902bab75638385b",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.669dc6ae.chunk.css"
+  },
+  {
+    "revision": "6ebcd91ebfa99b0d282d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.78e33ec8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.78e33ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46457902bab75638385b",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.d2574c82.chunk.js"
+  },
+  {
+    "revision": "2c33b5407f6ddb0305a4",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.395ab540.js"
+  }
+]);
